6cb5da410f15ec3cc981c3d6bcf118c2f74423e1
pipu11qiao <18854558196@163.com>
2018-04-22 12:04:16 +0800

    client_agent

--- js/professional_javascript_for_web_developers/code/CH09/client.js
+++ js/professional_javascript_for_web_developers/code/CH09/client.js
@@ -12,7 +12,6 @@ var client = function () {
     // 具体版本号
     ver: null
   };
-
   // 检测浏览器
   var browser = {
 
@@ -33,14 +32,28 @@ var client = function () {
   var system = {
     win: false,
     mac: false,
-    x11: false
+    x11: false,
+
+    // 移动设备
+    iphone: false,
+    ipod: false,
+    ipad: false,
+    ios: false,
+    android: false,
+    nokiaN: false,
+    winMobile: false,
+
+    // 游戏系统
+    wii: false,
+    ps: false
+
   };
 
-  var ua = navigtor.userAgent;
+  var ua = navigator.userAgent;
 
   // 开始检测
   if(window.opera) {
-    engine.ver = browser.ver = window.opera.verion();
+    engine.ver = browser.ver = window.opera.version();
     engine.opera = browser.opera = parseFloat(engine.ver);
   } else if (/AppleWebKit\/(\S+)/.test(ua)) {
     engine.ver = RegExp['$1'];
@@ -82,12 +95,78 @@ var client = function () {
     engine.ie = browser.ie =parseFloat(engine.ver);
   }
 
+  // 识别平台
+  var p = navigator.platform;
+  system.win = p.indexOf('Win') === 0;
+  system.mac = p.indexOf('Mac') === 0;
+  system.x11 = (p.indexOf('X11') === 0) || (p.indexOf('Linux') === 0);
+
+  // 识别windows
+  if(system.win){
+    if(/Win(?:dows)?([^do]{2})(\d+\.\d+)/.test(ua)) {
+      if(RegExp['$1'] === 'NT') {
+        switch (RegExp['$2']) {
+          case '5.0' :
+            system.win = '2000';
+            break;
+          case '5.1':
+            system.win = 'XP';
+            break;
+          case '6.0':
+            system.win = 'Vista';
+            break;
+          case '6.1':
+            system.win = 'NT';
+            break;
+          default:
+            system.win = 'NT';
+            break;
+        }
+      } else if (RegExp['$1'] === '9x'){
+        system.win = 'ME';
+      } else {
+        system.win = RegExp['$1'];
+      }
+    }
+  }
+
+  // 移动检测
+  system.iphone = ua.indexOf('iPhone') > -1;
+  system.ipod = ua.indexOf('iPod') > -1;
+  system.ipad = ua.indexOf('iPad') > -1;
+
+  if(system.mac && ua.indexOf('Mobile') > -1) {
+    if(/CPU (?:iPhone)?OS (\d+_\d+)/.test(ua)) {
+      system.ios = parseFloat(RegExp.$1.replace('-','.'));
+    } else {
+      system.ios = 2; // 不能识别
+    }
+  }
+  // 检测Andriod 版本
+  if(/Andriod (\d+\.\d+)/.test(ua)) {
+    system.android = parseFloat(RegExp.$1);
+  }
+  system.nokiaN = ua.indexOf('NokiaN') > -1;
+
+  // Window mobile
+  if(system.win === 'CE') {
+    system.winMobile = system.win;
+  } else if (system.win === 'Ph') {
+    if(/Windows Phone Os (\d+\.\d+)/.test(ua)) {
+      system.win = 'Phone';
+      system.winMobile = parseFloat(RegExp.$1);
+    }
+  }
+
+  // 游戏系统
+  system.wii = ua.indexOf('Wii') > -1;
+  system.ps = /playstation/i.test(ua);
 
   // 在此检测呈现引擎、平台和设备
   return {
     engine: engine,
 
-    bowser: browser,
+    browser: browser,
     system: system
   }
 }();
